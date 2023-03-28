0425f473521ededc0e7326605acf7a757cb3b9ae
SAcarsoy <saraacarsoy@sabanciuniv.edu>
2023-03-28 13:53:51 +0200

    Created a less folder and moved the file there

--- web-page/src/index.js
+++ web-page/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.less';
+import './less/index.less';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

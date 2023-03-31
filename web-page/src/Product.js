68f0dea56e92d21c73c9b033cea734494bd04a83
SAcarsoy <saraacarsoy@sabanciuniv.edu>
2023-03-31 16:38:22 +0200

    Added some styling and toggle for divider

--- web-page/src/Product.js
+++ web-page/src/Product.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Divider from './Divider';
 
 const Product = ({ product }) => {
+  const [showDetails, toggleDetails] = React.useState(false)
+  const onClick = () => toggleDetails(!showDetails)
+
   return (
-   <div className='product'>
+    <>
+    <div className='product'>
       <img src={require(`${product.imageSrc}`)} alt='Product' className='product__image'/>
       <div className='product__details'>     
         <span className='product__text'>Price: {product.price} kr</span>
+        <button className='order-button' onClick={onClick}></button>
       </div>
-      <Divider product={product}></Divider>
     </div>
+    { showDetails ? <Divider product={product}></Divider> : null }
+    </>
   );
 };
 
